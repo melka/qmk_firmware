efb303c95734978c06cd9d09e23e7e3419ab8c6f
Kamel Makhloufi <fabmanager@esaaa.fr>
2020-06-20 20:03:38 +0200

    Save before switching branch

--- keyboards/xd60/keymaps/melka/rules.mk
+++ keyboards/xd60/keymaps/melka/rules.mk
@@ -2,6 +2,6 @@ BOOTLOADER = qmk-dfu
 
 # Build Options
 CONSOLE_ENABLE = no         # Console for debug
-NRF24_ENABLE = yes
+NRF24_ENABLE = no
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 
